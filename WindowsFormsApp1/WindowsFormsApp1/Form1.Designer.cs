470aa2540c70ec159a072d46d1a1bae2114fcfed
namwlee99 <sideoftrump@pm.me>
2023-11-08 19:19:18 +0900

    1day end

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.button_div = new System.Windows.Forms.Button();
             this.button_in_min = new System.Windows.Forms.Button();
             this.button_plu = new System.Windows.Forms.Button();
+            this.button_ica = new System.Windows.Forms.Button();
+            this.button_log = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox_print
@@ -45,6 +47,7 @@
             this.textBox_print.Location = new System.Drawing.Point(13, 12);
             this.textBox_print.Multiline = true;
             this.textBox_print.Name = "textBox_print";
+            this.textBox_print.ReadOnly = true;
             this.textBox_print.Size = new System.Drawing.Size(518, 207);
             this.textBox_print.TabIndex = 0;
             // 
@@ -53,8 +56,9 @@
             this.textBox_input.Location = new System.Drawing.Point(12, 225);
             this.textBox_input.Multiline = true;
             this.textBox_input.Name = "textBox_input";
-            this.textBox_input.Size = new System.Drawing.Size(319, 238);
+            this.textBox_input.Size = new System.Drawing.Size(319, 299);
             this.textBox_input.TabIndex = 1;
+            this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
             // 
             // button_del
             // 
@@ -64,6 +68,7 @@
             this.button_del.TabIndex = 3;
             this.button_del.Text = "DEL";
             this.button_del.UseVisualStyleBackColor = true;
+            this.button_del.Click += new System.EventHandler(this.button_del_Click);
             // 
             // button_res
             // 
@@ -73,6 +78,7 @@
             this.button_res.TabIndex = 5;
             this.button_res.Text = "C";
             this.button_res.UseVisualStyleBackColor = true;
+            this.button_res.Click += new System.EventHandler(this.button_res_Click);
             // 
             // button_min
             // 
@@ -128,11 +134,31 @@
             this.button_plu.Text = "+";
             this.button_plu.UseVisualStyleBackColor = true;
             // 
+            // button_ica
+            // 
+            this.button_ica.Location = new System.Drawing.Point(436, 469);
+            this.button_ica.Name = "button_ica";
+            this.button_ica.Size = new System.Drawing.Size(94, 55);
+            this.button_ica.TabIndex = 12;
+            this.button_ica.Text = "=";
+            this.button_ica.UseVisualStyleBackColor = true;
+            // 
+            // button_log
+            // 
+            this.button_log.Location = new System.Drawing.Point(336, 469);
+            this.button_log.Name = "button_log";
+            this.button_log.Size = new System.Drawing.Size(94, 55);
+            this.button_log.TabIndex = 13;
+            this.button_log.Text = "Log";
+            this.button_log.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 475);
+            this.ClientSize = new System.Drawing.Size(538, 533);
+            this.Controls.Add(this.button_log);
+            this.Controls.Add(this.button_ica);
             this.Controls.Add(this.button_plu);
             this.Controls.Add(this.button_in_min);
             this.Controls.Add(this.button_div);
@@ -151,7 +177,6 @@
         }
 
         #endregion
-
         private System.Windows.Forms.TextBox textBox_print;
         private System.Windows.Forms.TextBox textBox_input;
         private System.Windows.Forms.Button button_del;
@@ -162,6 +187,9 @@
         private System.Windows.Forms.Button button_div;
         private System.Windows.Forms.Button button_in_min;
         private System.Windows.Forms.Button button_plu;
+        private System.Windows.Forms.Button button_ica;
+        private System.Windows.Forms.Button button_log;
+
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,37 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void textBox_input_TextChanged(object sender, EventArgs e)
+        {
+            if (!textBox_input.Text.Contains(","))
+            {
+                // 숫자로 변환하여 다시 형식화
+                if (double.TryParse(textBox_input.Text, out double number))
+                {
+                    textBox_input.Text.ToString();
+                }
+            }
+            textBox_print.Text = textBox_input.Text;
+        }
+
+        // 초기화 함수
+        private void button_res_Click(object sender, EventArgs e)
+        {
+            textBox_input.Text = "";
+        }
+
+        // 한글자씩 삭제 함수
+        private void button_del_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox_input.Text))
+            {
+                textBox_input.Text = textBox_input.Text.Substring(0, textBox_input.Text.Length - 1);
+            }
+        }
+        public static string AddCommasToNumber(double number)
+        {
+            return number.ToString("N0");
+        }
     }
 }
