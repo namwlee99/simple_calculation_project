6d5a164b5d9174c5338ef019960958388b6885b8
namwlee99 <sideoftrump@pm.me>
2023-11-09 18:22:41 +0900

    final

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -44,6 +44,8 @@
             // 
             // textBox_print
             // 
+            this.textBox_print.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox_print.Font = new System.Drawing.Font("Gulim", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.textBox_print.Location = new System.Drawing.Point(13, 12);
             this.textBox_print.Multiline = true;
             this.textBox_print.Name = "textBox_print";
@@ -53,118 +55,140 @@
             // 
             // textBox_input
             // 
+            this.textBox_input.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox_input.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.textBox_input.Location = new System.Drawing.Point(12, 225);
             this.textBox_input.Multiline = true;
             this.textBox_input.Name = "textBox_input";
-            this.textBox_input.Size = new System.Drawing.Size(319, 299);
+            this.textBox_input.Size = new System.Drawing.Size(319, 296);
             this.textBox_input.TabIndex = 1;
             this.textBox_input.TextChanged += new System.EventHandler(this.textBox_input_TextChanged);
             // 
             // button_del
             // 
+            this.button_del.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_del.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button_del.Location = new System.Drawing.Point(437, 225);
             this.button_del.Name = "button_del";
             this.button_del.Size = new System.Drawing.Size(94, 55);
             this.button_del.TabIndex = 3;
             this.button_del.Text = "DEL";
-            this.button_del.UseVisualStyleBackColor = true;
+            this.button_del.UseVisualStyleBackColor = false;
             this.button_del.Click += new System.EventHandler(this.button_del_Click);
             // 
             // button_res
             // 
+            this.button_res.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_res.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button_res.Location = new System.Drawing.Point(337, 225);
             this.button_res.Name = "button_res";
             this.button_res.Size = new System.Drawing.Size(94, 55);
             this.button_res.TabIndex = 5;
             this.button_res.Text = "C";
-            this.button_res.UseVisualStyleBackColor = true;
+            this.button_res.UseVisualStyleBackColor = false;
             this.button_res.Click += new System.EventHandler(this.button_res_Click);
             // 
             // button_min
             // 
-            this.button_min.Location = new System.Drawing.Point(337, 347);
+            this.button_min.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_min.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_min.Location = new System.Drawing.Point(437, 347);
             this.button_min.Name = "button_min";
             this.button_min.Size = new System.Drawing.Size(94, 55);
             this.button_min.TabIndex = 6;
             this.button_min.Text = "-";
-            this.button_min.UseVisualStyleBackColor = true;
+            this.button_min.UseVisualStyleBackColor = false;
             this.button_min.Click += new System.EventHandler(this.button_min_Click);
             // 
             // button_mul
             // 
-            this.button_mul.Location = new System.Drawing.Point(437, 347);
+            this.button_mul.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_mul.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_mul.Location = new System.Drawing.Point(337, 408);
             this.button_mul.Name = "button_mul";
             this.button_mul.Size = new System.Drawing.Size(94, 55);
             this.button_mul.TabIndex = 7;
             this.button_mul.Text = "*";
-            this.button_mul.UseVisualStyleBackColor = true;
+            this.button_mul.UseVisualStyleBackColor = false;
             this.button_mul.Click += new System.EventHandler(this.button_mul_Click);
             // 
             // button_rem
             // 
-            this.button_rem.Location = new System.Drawing.Point(437, 408);
+            this.button_rem.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_rem.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_rem.Location = new System.Drawing.Point(337, 471);
             this.button_rem.Name = "button_rem";
             this.button_rem.Size = new System.Drawing.Size(94, 55);
             this.button_rem.TabIndex = 8;
             this.button_rem.Text = "%";
-            this.button_rem.UseVisualStyleBackColor = true;
+            this.button_rem.UseVisualStyleBackColor = false;
             this.button_rem.Click += new System.EventHandler(this.button_rem_Click);
             // 
             // button_div
             // 
-            this.button_div.Location = new System.Drawing.Point(337, 408);
+            this.button_div.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_div.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_div.Location = new System.Drawing.Point(437, 408);
             this.button_div.Name = "button_div";
             this.button_div.Size = new System.Drawing.Size(94, 55);
             this.button_div.TabIndex = 9;
             this.button_div.Text = "/";
-            this.button_div.UseVisualStyleBackColor = true;
+            this.button_div.UseVisualStyleBackColor = false;
             this.button_div.Click += new System.EventHandler(this.button_div_Click);
             // 
             // log_reset
             // 
+            this.log_reset.BackColor = System.Drawing.Color.FloralWhite;
+            this.log_reset.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.log_reset.Location = new System.Drawing.Point(337, 286);
             this.log_reset.Name = "log_reset";
             this.log_reset.Size = new System.Drawing.Size(94, 55);
             this.log_reset.TabIndex = 10;
-            this.log_reset.Text = "Log_reset";
-            this.log_reset.UseVisualStyleBackColor = true;
+            this.log_reset.Text = "AC";
+            this.log_reset.UseVisualStyleBackColor = false;
             this.log_reset.Click += new System.EventHandler(this.log_reset_Click);
             // 
             // button_plu
             // 
-            this.button_plu.Location = new System.Drawing.Point(437, 286);
+            this.button_plu.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_plu.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_plu.Location = new System.Drawing.Point(337, 347);
             this.button_plu.Name = "button_plu";
             this.button_plu.Size = new System.Drawing.Size(94, 55);
             this.button_plu.TabIndex = 11;
             this.button_plu.Text = "+";
-            this.button_plu.UseVisualStyleBackColor = true;
+            this.button_plu.UseVisualStyleBackColor = false;
             this.button_plu.Click += new System.EventHandler(this.button_plu_Click);
             // 
             // button_ica
             // 
-            this.button_ica.Location = new System.Drawing.Point(436, 469);
+            this.button_ica.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_ica.Font = new System.Drawing.Font("Gulim", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_ica.Location = new System.Drawing.Point(437, 469);
             this.button_ica.Name = "button_ica";
             this.button_ica.Size = new System.Drawing.Size(94, 55);
             this.button_ica.TabIndex = 12;
             this.button_ica.Text = "=";
-            this.button_ica.UseVisualStyleBackColor = true;
+            this.button_ica.UseVisualStyleBackColor = false;
             this.button_ica.Click += new System.EventHandler(this.button_ica_Click);
             // 
             // button_log
             // 
-            this.button_log.Location = new System.Drawing.Point(336, 469);
+            this.button_log.BackColor = System.Drawing.Color.FloralWhite;
+            this.button_log.Font = new System.Drawing.Font("Gulim", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_log.Location = new System.Drawing.Point(437, 286);
             this.button_log.Name = "button_log";
             this.button_log.Size = new System.Drawing.Size(94, 55);
             this.button_log.TabIndex = 13;
             this.button_log.Text = "Log";
-            this.button_log.UseVisualStyleBackColor = true;
+            this.button_log.UseVisualStyleBackColor = false;
             this.button_log.Click += new System.EventHandler(this.button_log_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 533);
+            this.ClientSize = new System.Drawing.Size(538, 530);
             this.Controls.Add(this.button_log);
             this.Controls.Add(this.button_ica);
             this.Controls.Add(this.button_plu);
